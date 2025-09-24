8a1a793201d3b32b12f8314c5274206f90382b5d
Kavya P <kavyap2207@gmail.com>
2025-09-24 19:31:51 +0530

    Time: 2 ms (83.98%) | Memory: 45.3 MB (5.79%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,31 +1,19 @@
 class Solution {
-
-    public void help(int[] candidates,int target,int sum,int ind,ArrayList<Integer> temp,List<List<Integer>>result)
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        help(candidates,target,0,result,new ArrayList<>());
+        return result;
+    }
+    public void help(int[] candidates,int remain,int ind,List<List<Integer>> result,ArrayList<Integer> temp)
     {
-        if(sum==target){
+        if(remain==0) {
             result.add(new ArrayList<>(temp));
             return;
         }
-        if(sum>target) return;
-
-        for(int i=ind;i<candidates.length;i++)
-        {
-            sum+=candidates[i];
-            temp.add(candidates[i]);
-            help(candidates,target,sum,i,temp,result);
-            sum-=temp.get(temp.size()-1);
-            temp.remove(temp.size()-1);
-        }
-    }
-
-
-
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<>();
-        help(candidates,target,0,0,new ArrayList<>(),result);
-        return result;
-        
+        if(remain<0 || ind>=candidates.length) return;
+        temp.add(candidates[ind]);
+        help(candidates,remain-candidates[ind],ind,result,temp);
+        temp.remove(temp.size()-1);
+        help(candidates,remain,ind+1,result,temp);
     }
 }
