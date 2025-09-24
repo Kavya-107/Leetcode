456773adb1810ac8431d6fb3795dfa10b2b175eb
Kavya P <kavyap2207@gmail.com>
2025-09-24 14:16:36 +0530

    Time: 6 ms (15.08%) | Memory: 43.7 MB (14.43%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,24 +1,22 @@
 class Solution {
 
-    public void help(int sum,int[] candidates,int target,List<List<Integer>> result,ArrayList<Integer> temp,int ind)
+    public void help(int[] candidates,int target,List<List<Integer>> result,ArrayList<Integer> temp,int ind)
     {
-        if(sum==target)
+        if(target==0)
         {
             result.add(new ArrayList<>(temp));
             return;
         }
-        else if(sum>target)
+        else if(target<0)
         {
             return;
         }
 
         for(int i=ind;i<candidates.length;i++)
         {
-            if(i>ind  && candidates[i]==candidates[i-1]) continue;
             temp.add(candidates[i]);
-            sum+=candidates[i];
-            help(sum,candidates,target,result,temp,i+1);
-            sum-=temp.get(temp.size()-1);
+            help(candidates,target-candidates[i],result,temp,i+1);
+            while(i+1<candidates.length && candidates[i]==candidates[i+1]) i++;
             temp.remove(temp.size()-1);
         }
        
@@ -27,7 +25,7 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
-        help(0,candidates,target,result,new ArrayList<>(),0);
+        help(candidates,target,result,new ArrayList<>(),0);
         return result;
     }
 }
