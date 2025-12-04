71676f226e57c58000b5dabb442789056a33414a
Kavya P <kavyap2207@gmail.com>
2025-12-04 16:13:19 +0530

    Time: 3 ms (40.09%) | Memory: 47.8 MB (27.73%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(i>rd) return false;
             rd=Math.max(rd,i+nums[i]);
-            if(rd>=nums.length) return true;
+           // if(rd>=nums.length) return true;
         }
         return true;
     }
