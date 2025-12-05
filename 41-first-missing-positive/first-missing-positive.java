183b37d6677059b74fa154e90da93cf540ebf901
Kavya P <kavyap2207@gmail.com>
2025-12-05 09:40:49 +0530

    Time: 19 ms (11.25%) | Memory: 73.9 MB (14.35%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -3,15 +3,14 @@ class Solution {
         Arrays.sort(nums);
         int i=0;
         while(i<nums.length && nums[i]<=0) i++;
-        System.out.println("Out : "+i);
+       
         int k=1;
         while(i<nums.length && k == nums[i]) {
             if((i+1) < nums.length && nums[i+1]==k) k--;
             i++;
             k++;
         }
-        System.out.println("i : "+i+"k :"+k);
-
+       
         return k;
     }
 }
